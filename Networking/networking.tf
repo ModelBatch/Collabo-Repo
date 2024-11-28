eac074e880c3689b5a64860a3bc12b2fa24ea542
larasgit <lara2k@hotmail.com>
2024-11-27 20:00:31 -0500

    subnet-creation

--- Networking/networking.tf
+++ Networking/networking.tf
@@ -29,3 +29,23 @@ resource "aws_internet_gateway_attachment" "Collabo-att" {
   internet_gateway_id = aws_internet_gateway.Collabo-igw.id
   vpc_id              = aws_vpc.Collabo-Repo-vpc.id
 }
+
+#2 Create public Subnet
+resource "aws_subnet" "pub-subnet" {
+  vpc_id     = aws_vpc.Collabo-Repo-vpc.id
+  cidr_block = "172.20.1.0/24"
+
+  tags = {
+    Name = "Main-public-subnet"
+  }
+}
+
+#2 Create private Subnet
+resource "aws_subnet" "pub-subnet" {
+  vpc_id     = aws_vpc.Collabo-Repo-vpc.id
+  cidr_block = "172.20.2.0/24"
+
+  tags = {
+    Name = "Main-private-subnet"
+  }
+}
