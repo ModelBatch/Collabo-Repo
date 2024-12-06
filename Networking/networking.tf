8fd02f301907f16e265a4b471e0264888bf61156
mahbuba khanam <khanmahbuba.2021@gmail.com>
2024-12-06 04:26:34 +0100

    natgateway creation

--- Networking/networking.tf
+++ Networking/networking.tf
@@ -67,3 +67,20 @@ resource "aws_route_table_association" "Pub-rt" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.Pub-rt.id
 }
+
+resource "aws_eip" "Collabo-NAT" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "Collabo-NAT" {
+allocation_id = aws_eip.Collabo-NAT.id
+subnet_id     = aws_subnet.public-subnet.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.Collabo-igw]
+}
