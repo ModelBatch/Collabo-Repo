883d1837571b077afae9ab21257b3e5a3938bcc9
Prince <etiennekum12@gmail.com>
2024-12-18 20:52:59 -0700

    Autoscaling-group and launch template for web and app tier

--- Networking/networking.tf
+++ Networking/networking.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.77.0"
     }
   }
@@ -67,7 +67,7 @@ resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.Collabo-Repo-vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.Collabo-NAT.id
   }
 
@@ -91,8 +91,8 @@ resource "aws_eip" "Collabo-NAT" {
 }
 
 resource "aws_nat_gateway" "Collabo-NAT" {
-allocation_id = aws_eip.Collabo-NAT.id
-subnet_id     = aws_subnet.public-subnet.id
+  allocation_id = aws_eip.Collabo-NAT.id
+  subnet_id     = aws_subnet.public-subnet.id
 
   tags = {
     Name = "gw NAT"
@@ -132,7 +132,7 @@ resource "aws_network_acl" "public-acl" {
     rule_no    = 120
     action     = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 0  
+    from_port  = 0
     to_port    = 0
   }
 }
@@ -145,7 +145,7 @@ resource "aws_network_acl" "priv-acl" {
 
   ingress {
     protocol   = "-1"
-    rule_no    = 100  
+    rule_no    = 100
     action     = "deny"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
@@ -156,7 +156,7 @@ resource "aws_network_acl" "priv-acl" {
     protocol   = "-1"
     rule_no    = 100
     action     = "deny"
-    cidr_block = "0.0.0.0/0"  
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
     to_port    = 0
   }
@@ -178,23 +178,23 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- 
+
   ingress {
     description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]      
-    }
- 
- ingress  {
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "SSH from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]      
-    }
-    egress {
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -213,7 +213,7 @@ resource "aws_security_group" "app-sg" {
   description = "Allow HTTPS and HTTP inbound traffic"
   vpc_id      = aws_vpc.Collabo-Repo-vpc.id
 
-ingress {
+  ingress {
     description = "TLS from VPC"
     from_port   = 443
     to_port     = 443
@@ -221,16 +221,16 @@ ingress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- 
+
   ingress {
     description = "HTTP from VPC"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]      
-    }
- 
-    egress {
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -249,22 +249,156 @@ resource "aws_security_group" "db-sg" {
   description = "Allow inbound traffic on port 3306"
   vpc_id      = aws_vpc.Collabo-Repo-vpc.id
 
-ingress {
+  ingress {
     description = "TLS from VPC"
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["172.20.0.0/20"]
-  }     
+  }
 
-    egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    }   
+  }
 
-    tags = {
+  tags = {
     Name = "database-sg"
-    } 
+  }
 }
+
+
+data "aws_ami" "linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+# launch template for web tier
+
+resource "aws_launch_template" "web-launch-template" {
+  name_prefix   = "web-launch-template"
+  image_id      = data.aws_ami.linux.id
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web-sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "web-instance"
+    }
+  }
+}
+
+# Auto scaling group for web tier
+
+resource "aws_autoscaling_group" "web-asg" {
+  name             = "web-asg"
+  desired_capacity = 1
+  max_size         = 2
+  min_size         = 1
+  launch_template {
+    id      = aws_launch_template.web-launch-template.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = [aws_subnet.public-subnet.id]
+}
+
+# launch ec2 instance for web tier
+
+resource "aws_instance" "web-instance" {
+  ami           = data.aws_ami.linux.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public-subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  key_name = "Demo-key"
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("~/.ssh/Demo-key.pem")
+    host = self.public_ip
+  }
+  tags = {
+    Name = "web-instance" 
+  }
+}
+
+# launch template for app tier
+
+resource "aws_launch_template" "app-launch-template" {
+  name_prefix   = "app-launch-template"
+  image_id      = data.aws_ami.linux.id
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.app-sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "app-instance"
+    }
+  }
+}
+
+# Auto scaling group for app tier
+
+resource "aws_autoscaling_group" "app-asg" {
+  name             = "app-asg"
+  desired_capacity = 1
+  max_size         = 2
+  min_size         = 1
+  launch_template {
+    id      = aws_launch_template.app-launch-template.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = [aws_subnet.private-subnet.id]    
+}
+
+# launch ec2 instance for app tier
+
+resource "aws_instance" "app-instance" {
+  ami           = data.aws_ami.linux.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private-subnet.id
+  vpc_security_group_ids = [aws_security_group.app-sg.id]
+
+  tags = {
+    Name = "app-instance" 
+  }
+}
+  
